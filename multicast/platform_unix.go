0eacca1432c88e44d966f67fc994d0ce73344841
vadym <vadym.vikulin@gmail.com>
2021-05-06 18:54:05 +0300

    1. corrected code to build for windows

--- multicast/platform_unix.go
+++ multicast/platform_unix.go
@@ -12,20 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !linux,!darwin,!netbsd,!freebsd,!openbsd,!dragonflybsd,!windows
+// +build linux netbsd freebsd openbsd dragonflybsd
 
 package multicast
 
 import (
 	"fmt"
 	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 func (m *Multicast) multicastStarted() {
 }
 
 func (m *Multicast) udpOptions(network string, address string, c syscall.RawConn) error {
-	return nil
+	var reuseport error
+	control := c.Control(func(fd uintptr) {
+		reuseport = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+	})
+
+	switch {
+	case reuseport != nil:
+		return fmt.Errorf("SO_REUSEPORT: %w", reuseport)
+	default:
+		return control
+	}
 }
 
 func (m *Multicast) tcpOptions(network string, address string, c syscall.RawConn) error {
