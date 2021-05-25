be0f11cea79ad2986bc097688d06f09452d37e8c
Neil Alexander <neilalexander@users.noreply.github.com>
2021-05-25 17:28:08 +0100

    Don't check for tree announcements in interval

--- router/router.go
+++ router/router.go
@@ -406,18 +406,20 @@ func (r *Router) Connect(conn net.Conn, public types.PublicKey, zone string, pee
 		r.active.Store(hex.EncodeToString(public[:])+zone, i)
 		go r.callbacks.onConnected(i, public, peertype)
 		go r.snake.portWasConnected(i)
-		if i != 0 {
-			go func(r *Router, p *Peer) {
-				select {
-				case <-p.context.Done():
-					// port's already dead so give up
-				case <-time.After(announcementInterval):
-					if !p.alive.Load() {
-						_ = r.Disconnect(p.port, fmt.Errorf("timed out waiting for tree announcement"))
+		/*
+			if i != 0 {
+				go func(r *Router, p *Peer) {
+					select {
+					case <-p.context.Done():
+						// port's already dead so give up
+					case <-time.After(announcementInterval):
+						if !p.alive.Load() {
+							_ = r.Disconnect(p.port, fmt.Errorf("timed out waiting for tree announcement"))
+						}
 					}
-				}
-			}(r, r.ports[i])
-		}
+				}(r, r.ports[i])
+			}
+		*/
 		r.log.Printf("Connected port %d to %s (zone %q)\n", i, conn.RemoteAddr(), zone)
 		return i, nil
 	}
