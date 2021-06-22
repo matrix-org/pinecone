6e630fb016ac17f1caab362246e32fb3175106fe
Neil Alexander <neilalexander@users.noreply.github.com>
2021-06-22 12:17:27 +0100

    Revert "Update pineconeip to work on partial matching"
    This reverts commit 41a80538100fda66b5733edbea19095bd3f43878.

--- cmd/pineconeip/tun/tun.go
+++ cmd/pineconeip/tun/tun.go
@@ -1,10 +1,8 @@
 package tun
 
 import (
-	"context"
 	"fmt"
 	"net"
-	"sync"
 
 	"github.com/matrix-org/pinecone/router"
 	"github.com/matrix-org/pinecone/types"
@@ -15,19 +13,13 @@ import (
 const TUN_OFFSET_BYTES = 4
 
 type TUN struct {
-	r             *router.Router
-	iface         wgtun.Device
-	partialToFull map[types.PublicKey]types.PublicKey
-	mutex         sync.RWMutex
+	r     *router.Router
+	iface wgtun.Device
+	//	partialToFull map[types.PublicKey]types.PublicKey
+	//	mutex         sync.RWMutex
 }
 
-var partialMask = types.PublicKey{
-	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-}
-
+/*
 func (t *TUN) Lookup(partial types.PublicKey) types.PublicKey {
 	t.mutex.RLock()
 	pk, ok := t.partialToFull[partial]
@@ -35,7 +27,7 @@ func (t *TUN) Lookup(partial types.PublicKey) types.PublicKey {
 	if ok {
 		return pk
 	}
-	full, addr, err := t.r.DHTSearch(context.Background(), partial[:], partialMask[:], false)
+	full, addr, err := t.r.DHTSearch(context.Background(), partial[:], false)
 	if err != nil {
 		fmt.Println("DHT search failed:", err)
 		return types.PublicKey{}
@@ -46,6 +38,7 @@ func (t *TUN) Lookup(partial types.PublicKey) types.PublicKey {
 	t.mutex.Unlock()
 	return full
 }
+*/
 
 func AddressForPublicKey(pk types.PublicKey) net.IP {
 	a := [16]byte{0xFD}
@@ -64,8 +57,8 @@ func PublicKeyForAddress(a net.IP) types.PublicKey {
 
 func NewTUN(r *router.Router) (*TUN, error) {
 	t := &TUN{
-		r:             r,
-		partialToFull: make(map[types.PublicKey]types.PublicKey),
+		r: r,
+		//	partialToFull: make(map[types.PublicKey]types.PublicKey),
 	}
 	addr := AddressForPublicKey(r.PublicKey())
 	if err := t.setup(addr); err != nil {
@@ -94,8 +87,7 @@ func (t *TUN) read() {
 		if dst[0] != 0xFD {
 			continue
 		}
-		//ns, err := t.r.WriteTo(bs, pk)
-		ns, err := t.r.WriteTo(bs, t.Lookup(pk))
+		ns, err := t.r.WriteTo(bs, pk)
 		if err != nil {
 			fmt.Println("t.r.WriteTo:", err)
 			continue
